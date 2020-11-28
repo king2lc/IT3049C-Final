ffa60546a9c2fda0432d029a9aaef8056b0b82f8
Eric Miller <54557245+Emiller321@users.noreply.github.com>
2020-11-28 12:09:53 -0500

    Creates the keyboard

--- scene1.js
+++ scene1.js
@@ -19,6 +19,8 @@ class Scene1 extends Phaser.Scene {
       this.bg = this.add.image(windowWidth / 2, windowHeight / 2, 'background');
       this.bg.setDisplaySize(windowWidth, windowHeight);
       this.player = this.physics.add.sprite(100, 100, 'player');
+      this.arrow = this.input.keyboard.createCursorKeys();
+
     }
   }
   
