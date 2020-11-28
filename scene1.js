3a4d72c1142c2524e559f5d9019b6a37944565f0
Eric Miller <54557245+Emiller321@users.noreply.github.com>
2020-11-28 18:27:56 -0500

    Changed height and width of player

--- scene1.js
+++ scene1.js
@@ -8,7 +8,10 @@ class Scene1 extends Phaser.Scene {
 
         this.load.image('test', 'Assets/Asteroids, Meteors/filled/Stones2Filled_01.png');
 
-        this.load.image('player', 'Assets/!!!ASSETSINUSE!!!/player.png');
+      this.load.image('player', 'Assets/!!!ASSETSINUSE!!!/player.png',{
+        frameWidth: 100,
+        frameHeight: 100
+      });
         
         this.load.spritesheet("ship", "Assets/!!!ASSETSINUSE!!!/Pro.png",{
             frameWidth: 16,
