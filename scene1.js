a2b86e09c54fdd22aaf0791e53cef9980e26bf7d
Eric Miller <54557245+Emiller321@users.noreply.github.com>
2020-11-28 18:06:22 -0500

    created player in preload

--- scene1.js
+++ scene1.js
@@ -6,7 +6,10 @@ class Scene1 extends Phaser.Scene {
     preload(){
         this.load.image('background', 'Assets/!!!ASSETSINUSE!!!/bg9.png');
 
-        this.load.image('player', 'Assets/!!!ASSETSNUSE!!!/ShipOrig_0003_Package-----------------.png');
+        this.load.spritesheet('player', 'Assets/!!!ASSETSNUSE!!!/player.png',{
+          frameWidth: 16,
+          frameHeight: 24
+        });
         
         this.load.spritesheet("ship", "Assets/!!!ASSETSINUSE!!!/Pro.png",{
             frameWidth: 16,
