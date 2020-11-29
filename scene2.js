6ec474f883378b052f8c85a2f49d1e897cf71d5c
Joseph Monahan <monahajc@mail.uc.edu>
2020-11-29 18:45:30 -0500

    earth

--- scene2.js
+++ scene2.js
@@ -11,7 +11,7 @@ class Scene2 extends Phaser.Scene {
       //this.add.image() adds the image 
       this.bg = this.add.image(windowWidth / 2, windowHeight / 2, 'background');
       this.bg.setDisplaySize(windowWidth, windowHeight);
-      var image = this.add.image(0, config.height / 2, 'earth');
+      var image = this.add.image(-500, config.height / 2, 'earth');
       player = this.physics.add.image(config.width / 2, config.height / 2, "player");
       this.meteor1 = this.add.image(config.width, config.height/2 - 150, "meteor1");
       this.meteor2 = this.add.image(config.width, config.height/2 - 50, "meteor2");
