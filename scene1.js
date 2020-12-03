a7eca695a6d740d250a9ad6ac868460665540a20
Eric Miller <54557245+Emiller321@users.noreply.github.com>
2020-12-03 14:57:13 -0500

    Explosion

--- scene1.js
+++ scene1.js
@@ -18,6 +18,11 @@ class Scene1 extends Phaser.Scene {
         frameHeight: 100
       });
         
+      this.load.spritesheet("explosion", "assets/spritesheets/explosion.png", {
+        frameWidth: 16,
+        frameHeight: 16
+    });
+
       this.load.image("earth", "Assets/!!!ASSETSINUSE!!!/Earth.png",{
         frameWidth: 100,
         frameHeight: 100
@@ -50,6 +55,14 @@ class Scene1 extends Phaser.Scene {
     this.add.text(200, 200, "Loading...");
 
         this.scene.start("playGame");
+
+        this.anims.create({
+          key: "explode",
+          frames: this.game.anims.generateFrameNumbers("explosion"),
+          frameRate: 20,
+          repeat: 0,
+          hideOnComplete: true
+      });
 
     }
   }
